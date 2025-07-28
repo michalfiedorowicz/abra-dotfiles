29cd4e47e35cea39376bf99da9dea44d676aec12
Michał Fiedorowicz <kontakt.mfiedorowicz@gmail.com>
2025-07-28 23:35:55 +0200

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,4 +3,4 @@ pkill -x polybar 2>/dev/null
 while pgrep -x polybar >/dev/null; do sleep 0.2; done
 
 MONITOR_PRIMARY=$(polybar --list-monitors | head -n1 | cut -d: -f1)
-MONITOR=$MONITOR_PRIMARY polybar topbar 2>&1 | tee -a /tmp/polybar.log &
+MONITOR=$MONITOR_PRIMARY polybar topbar  & disown
